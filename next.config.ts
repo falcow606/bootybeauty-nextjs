f43d015194679ba14753e04d52bd2324ecb7cdbf
Quentin Falcomer <quentin.falcomer@gmail.com>
2025-10-18 17:27:09 +0200

    chore: allow Joom/JoomCDN images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,8 +12,14 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'www.yodibeauty.com' },
       { protocol: 'https', hostname: 'yodibeauty.com' },
       { protocol: 'https', hostname: 'cdn.shopify.com' },
-
+      // Joom 
+      { protocol: 'https', hostname: 'www.joom.com' },
+      { protocol: 'https', hostname: 'img.joomcdn.ne' },
+      
       // (ajoute ici d’autres marchands si besoin)
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "www.laroche-posay.fr", pathname: "/**" },
+      { protocol: "https", hostname: "laroche-posay.fr",       pathname: "/**" },
     ],
   },
 };
